4a996df748fb44a8c0d9e3e10fa313cb22a2364d
Martina Ambrosini <martinaambrosini17@gmail.com>
2024-01-25 16:45:18 +0100

    add: added todo and todoapi in to-do-list.vue

--- Front-end/todos/src/components/To-do-list.vue
+++ Front-end/todos/src/components/To-do-list.vue
@@ -1,9 +1,29 @@
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'To-do-list'
+    name: 'To-do-list',
+    data() {
+        return {
+            todos: []
+        }
+    },
+    mounted() {
+        const t = this;
+        axios.get('http://localhost/TodoApi.php')
+            .then(res => {
+                t.todos = res.data;
+            }).catch(err => console.error(err));
+    }
 }
+
 </script>
 
 <template>
     <h1>ToDo List: </h1>
+    <ul>
+        <li v-for="todo in todos">
+            {{ todo.task }}
+        </li>
+    </ul>
 </template>
